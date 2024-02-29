56818b9630ac1eebd42f8fbd277106192a47a3eb
weicksa <75697263+weicksa@users.noreply.github.com>
2024-02-29 10:34:10 +0100

    Add files via upload

---
+++ models/model.py
@@ -0,0 +1,13 @@
+# TODO
+# Model architecture
+# evaluation function
+# train function
+
+# RNN network
+
+# CNN network
+
+# others ??
+
+
+# multilabel classification or 4 classes or 2 seperate networks ?
